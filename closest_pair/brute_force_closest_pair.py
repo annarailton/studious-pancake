ecc1b35e8227c7426396bc78e63047a8f41b25c8
railton <anna.railton@gmail.com>
2018-10-09 12:21:36 +0100

    Add lazy evaluation version of brute force
    Add progress bar

--- closest_pair/brute_force_closest_pair.py
+++ closest_pair/brute_force_closest_pair.py
@@ -1,4 +1,10 @@
+import itertools
 import numpy as np
+import time
+
+from tqdm import tqdm
+
+from scipy.special import comb
 
 
 def dist_euclidean(x, y):
@@ -11,6 +17,7 @@ def brute_force_closest_pair(points, dist):
 
     Args:
         points (list): list of points with some distance metric between them
+        dist (func): function that takes two points and returns a distance
 
     Returns:
         min_distance: minimum distance between points
@@ -33,3 +40,38 @@ def brute_force_closest_pair(points, dist):
                 min_points = tuple([points[i, :], points[j, :]])
 
     return min_distance, min_points
+
+
+def brute_force_lazy(points, dist):
+    """
+    Brute force closest pair algorithm using lazy evaluation to save on memory
+    for big sets of points.
+
+    Args:
+        points (list): list of points with some distance metric between them
+        dist (func): function that takes two points and returns a distance
+
+    Returns:
+        min_distance: minimum distance between points
+        min_points: set of pair of points that are closest_pair
+    """
+    if len(points) == 1:
+        return None, None
+
+    min_distance = np.inf
+    min_points = None
+    n_pairs = comb(len(points), 2, exact=True)
+    pairs = itertools.combinations(points, 2)
+    for p in tqdm(pairs, total=n_pairs, desc="brute_force_lazy"):
+        p_dist = dist(*p)
+        if p_dist < min_distance:
+            min_distance = p_dist
+            min_points = tuple(p)
+
+    return min_distance, min_points
+
+
+if __name__ == '__main__':
+    points = np.random.random(size=[4000, 3])
+    print(brute_force_lazy(points, dist_euclidean))
+    # print(brute_force_closest_pair(points, dist_euclidean))
