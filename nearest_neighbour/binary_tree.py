8dc3029b5e48484e7fbd2a04b9c35ea2829665cd
railton <anna.railton@gmail.com>
2018-10-17 15:32:39 +0100

    Add data to nodes, searching and min/max

--- nearest_neighbour/binary_tree.py
+++ nearest_neighbour/binary_tree.py
@@ -1,30 +1,40 @@
 """Recursive definition of a binary search tree."""
 import numpy as np
 
+from num2words import num2words
+
 
 class Node(object):
     """Node in a binary search tree"""
 
-    def __init__(self, location, left_child, right_child):
-        self.location = location
-        self.left_child = left_child
-        self.right_child = right_child
+    def __init__(self, key, left, right, data=None):
+        self.key = key
+        self.left = left
+        self.right = right
+        self.data = data
 
     def __repr__(self):
-        return "({}, {}, {})".format(self.location, self.left_child,
-                                     self.right_child)
-
+        return "({}, {}, {}, {})".format(self.key, self.data,
+                                         self.left, self.right)
 
 def build_tree(point_list):
     """Recursively build a binary search tree."""
     if not point_list:
         return None
 
-    root = point_list.pop(0)
-    return Node(
-        location=root,
-        left_child=build_tree([n for n in point_list if n <= root]),
-        right_child=build_tree([n for n in point_list if n > root]))
+    try:
+        root_loc, root_data = point_list.pop(0)
+        return Node(
+            key=root_loc,
+            left=build_tree([n for n in point_list if n[0] <= root_loc]),
+            right=build_tree([n for n in point_list if n[0] > root_loc]),
+            data=root_data)
+    except ValueError:  # no datafield
+        root_loc = point_list.pop(0)
+        return Node(
+            key=root_loc,
+            left=build_tree([n for n in point_list if n <= root_loc]),
+            right=build_tree([n for n in point_list if n > root_loc]))
 
 
 def inorder_traverse(node):
@@ -32,9 +42,9 @@ def inorder_traverse(node):
     subtrees."""
     if node is None:
         return
-    inorder_traverse(node.left_child)
-    print(node.location)
-    inorder_traverse(node.right_child)
+    inorder_traverse(node.left)
+    print(node.key)
+    inorder_traverse(node.right)
 
 
 def preorder_traverse(node):
@@ -42,9 +52,9 @@ def preorder_traverse(node):
     subtrees. Topologically sorted."""
     if node is None:
         return
-    print(node.location)
-    preorder_traverse(node.left_child)
-    preorder_traverse(node.right_child)
+    print(node.key)
+    preorder_traverse(node.left)
+    preorder_traverse(node.right)
 
 
 def postorder_traverse(node):
@@ -52,33 +62,65 @@ def postorder_traverse(node):
     subtrees."""
     if node is None:
         return
-    postorder_traverse(node.left_child)
-    postorder_traverse(node.right_child)
-    print(node.location)
+    postorder_traverse(node.left)
+    postorder_traverse(node.right)
+    print(node.key)
 
 
 def tree_search(node, key):
-    pass
+    """Recursively search the tree for the node with key `key`"""
+    if node is None or key == node.key:
+        return node
+    if key < node.key:
+        return tree_search(node.left, key)
+    return tree_search(node.right, key)
+
 
 
 def tree_search_iterative(node, key):
-    pass
+    """Unroll the recursion and iteratively search the tree for the node with
+    key `key`"""
+    while node is not None and key != node.key:
+        if key < node.key:
+            node = node.left
+        else:
+            node = node.right
+    return node
 
 
 def tree_minimum(node):
-    pass
+    """Follow the left children to find the minimum value in the tree."""
+    while node.left:
+        node = node.left
+    return node
 
 
-def tree_maximum():
-    pass
+def tree_maximum(node):
+    """Follow the left children to find the minimum value in the tree."""
+    while node.right:
+        node = node.right
+    return node
 
 
 def main():
     numbers = np.random.choice(range(100), size=20, replace=False).tolist()
-    tree = build_tree(numbers)
-    print(tree)
-    inorder_traverse(tree)
-    preorder_traverse(tree)
+    points_with_data = list(zip(numbers, map(num2words, numbers)))
+    tree = build_tree(points_with_data)
+
+    for k in range(40, 50):
+        found_node = tree_search(tree, k)
+        if found_node is not None:
+            print(k, found_node.data)
+
+    for k in range(50, 60):
+        found_node = tree_search_iterative(tree, k)
+        if found_node is not None:
+            print(k, found_node.data)
+
+    tree_min = tree_minimum(tree)
+    tree_max = tree_maximum(tree)
+    print("Tree minimum: ", tree_min.key, tree_min.data)
+    print("Tree maximum: ", tree_max.key, tree_max.data)
 
 
 if __name__ == '__main__':
